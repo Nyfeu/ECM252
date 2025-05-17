cb03c220ad1e4a8adc1baf5760cec4603d38bdcb
ANDRE SOLANO FERREIRA RODRIGUES MAIOLINI <19.02012-0@maua.br>
2025-05-17 12:54:18 -0300

    learn(08-react): encerra a secao 2.10

---
+++ react-hooks/src/components/Accordion.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Card } from 'primereact/card'
+import './Accordion.css'
+
+const Accordion = ({ itens }) => {
+
+    const [activeIndex, setActiveIndex] = useState(null) 
+
+    const _clickedItem = (key) => {
+        
+        // Mostra o item
+        console.log(key)
+
+        // Atualizar a variável de estado atribuindo o indice a ela
+        setActiveIndex(key === activeIndex ? null : key)
+
+    }
+
+    const _constructCard = (item, key) => {
+    
+        const verifyContent = (key) => {
+            return (key === activeIndex) ? {display: 'block'} : {display: 'none'}
+        }
+
+        const verifyIcon = (key) => {
+            return (key === activeIndex) ? 'pi pi-angle-down' : 'pi pi-angle-right'
+        }
+
+        return (
+    
+            <Card id='accordion' key={key} className='border-1 border-400 my-3'>
+
+                <div onClick={() => _clickedItem(key)}>
+                    <i className={verifyIcon(key)}></i>
+                    <h5 className='inline ml-3'>{item.titulo}</h5>
+                </div>
+
+                <div style={verifyContent(key)}>
+                    <p>{item.conteudo}</p>
+                </div>
+
+            </Card>
+
+        )
+
+    }
+
+    const _constructJSX = () => itens.map((item, key) => _constructCard(item, key))
+
+    return (
+        <>
+            {_constructJSX()}
+        </>
+    )
+}
+
+export default Accordion
